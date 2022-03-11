506b6afe45c64206eddd4bc7d8490fbd118c9815
Vatican22 <Vatican22@gmail.com>
2022-03-11 13:31:52 -0500

    added saveJSON() and formatted
    Need to test, to see if saveJSON() resolves the error we're seeing when writing to an empty or non existing file.

--- src/dal/DataManager.js
+++ src/dal/DataManager.js
@@ -7,100 +7,105 @@ dotenv.config()
 
 /* Handles any database calls */
 
-class DataManager
-{
+class DataManager {
     #connectionString;
-    constructor()
-    {
+    constructor() {
         this.#connectionString = process.env.URI;
     }
 
-    connectDB()
-    {
-        mongoose.connect(this.#connectionString, {keepAlive:true});
+    connectDB() {
+        mongoose.connect(this.#connectionString, { keepAlive: true });
     }
 
-    disconnectDB()
-    {
+    disconnectDB() {
         mongoose.connection.close()
     }
 
-    getConnectionString()
-    {
+    getConnectionString() {
         return this.#connectionString;
     }
 
-    createProfile(user)
-    {
+    createProfile(user) {
         setTimeout(async () => {
             await new profileModel({
                 userName: user.username
-                ,userId : user.id
-                ,mmr: 0
-                ,wins: 0
-                ,losses: 0
-                ,rank: 0
-                ,rankRole: 0
-                ,emblem: ' '
-                ,gamerTag: ' '
+                , userId: user.id
+                , mmr: 0
+                , wins: 0
+                , losses: 0
+                , rank: 0
+                , rankRole: 0
+                , emblem: ' '
+                , gamerTag: ' '
             }).save()
         }, 1000)
     }
 
-    async getProfiles(){
+    async getProfiles() {
         // fetch all profiles
         const profiles = await profileModel.find()
         return profiles;
     }
 
-    async getProfileById(id){
+    async getProfileById(id) {
         const profile = await profileModel.findOne({ userId: id })
         return profile;
     }
 
-    async createQueue(user, interactionId, rank){
+    saveJSON(filename = '', json = '""') {
+        return fs.writeFileSync(filename, JSON.stringify(json, null, 2), err => {
+            if (err) {
+                throw err;
+            }
+        })
+    }
+
+
+    async createQueue(user, interactionId, rank) {
         // checkFilesExists()
         // if not create file queues.json and add queue
         // if so add queue
         const profile = await this.getProfileById(user.id)
         const queue = new queueModel(profile, interactionId, rank)
-        const jsonQueue = JSON.stringify(queue)
-        fs.writeFile('queues.json', jsonQueue, err => {
-            if(err){
-                throw err;
-            }
-        })
+        saveJSON('queues.json', queue);
+        // const jsonQueue = JSON.stringify(queue)
+        // fs.writeFile('queues.json', jsonQueue, err => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        // })
     }
 
-    async joinQueue(user, interactionId, rank){
+    async joinQueue(user, interactionId, rank) {
         const selectedQueue = this.getQueueByRank(rank)
         var profile = new profileModel()
-        profile = await this.getProfileById(user.id)        
+        profile = await this.getProfileById(user.id)
         selectedQueue.players.push(profile)
-        const jsonQueue = JSON.stringify(selectedQueue)
-        fs.writeFile('queues.json', jsonQueue, err => {
-            if(err){
-                throw err;
-            }
-        })
+        saveJSON('queues.json', selectedQueue);
+        // const jsonQueue = JSON.stringify(selectedQueue)
+        // fs.writeFile('queues.json', jsonQueue, err => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        // })
         return selectedQueue
-    } 
+    }
 
 
-    getQueues(){
+    getQueues() {
         const queues = []
         queues.push(JSON.parse(fs.readFileSync('queues.json')))
         return queues
     }
 
-    getQueueByRank(rank){
+    getQueueByRank(rank) {
         const queues = []
         queues.push(JSON.parse(fs.readFileSync('queues.json')))
         const selectedQueue = queues.find(queue => { return queue.rank === rank })
         return selectedQueue
     }
 
-    getQueueById(id){
+    getQueueById(id) {
         const queues = []
         queues.push(JSON.parse(fs.readFileSync('queues.json')))
         const selectedQueue = queues.find(queue => { return queue.queueId === id })
